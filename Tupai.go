076808f16e2df1220513d02011e8c44a7846cd6f
ihsanul14 <ihsanboga6415@gmail.com>
2020-05-26 09:02:10 +0700

    Tes tupai

---
+++ Tupai.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var tes string = "Galadia Tupai"
+	// fmt.Println(tes)
+	// fmt.Println(tes[:4])
+	// fmt.Println(tes[0])
+	// nilai := 2
+	// // fmt.Println(nilai)
+	// for i := 1; i <= 10; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Println("Alum")
+	// 	} else if i%3 == 0 {
+	// 		fmt.Println("Alum Lai")
+	// 	} else if i%5 == 0 {
+	// 		fmt.Println("Alum Nyinyia")
+	// 	} else {
+	// 		fmt.Println(i)
+	// 	}
+	// }
+	// fmt.Println("Alah du")
+
+	// var names [3]string
+	// text := "text"
+	// for i := 0; i < len(names); i++ {
+
+	// 	names[i] = text
+	// 	text = text + " again"
+	// }
+	// for _, value := range names {
+	// fmt.Println(value)
+	// }
+
+	// mahasiswa := make(map[int]string)
+	// mahasiswa[1] = "Tupai"
+	// mahasiswa[2] = "Tupaidar"
+	// mahasiswa[3] = "Tupaidaratlaut"
+
+	// for nim, name := range names {
+	// 	fmt.Println(nim, " ", name)
+	// }
+	fmt.Println("Tupai")
+}
